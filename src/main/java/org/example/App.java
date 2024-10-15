f530d00ef686f5b0b2ffcf9a13ca5cc60054f4b4
AlexanderD <dementyev91@gmail.com>
2024-10-15 12:05:32 +0300

    1st commit after recreacting repo

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Enter two int numbers and get ur result of" +
+                " multiplication");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Result: " + a * b);
+    }
+}
