a5a12c2c21afacb8137b183b5ae392530a166cbf
AlexanderD <dementyev91@gmail.com>
2024-10-15 16:50:18 +0300

    New branch commit for GH

--- src/main/java/org/example/SpecialClassForGitGubPushing.java
+++ src/main/java/org/example/SpecialClassForGitGubPushing.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class SpecialClassForGitGubPushing {
     public static void main(String[] args) {
-        System.err.println("Yeah, it's realy special class");
+        System.err.println("Yeah, it's really special class");
+        System.out.println("We'll branching or forking our project");
     }
 }
