0b56a54f8d7ca7f9614f565dfba8094908550543
AlexanderD <dementyev91@gmail.com>
2024-10-15 13:26:50 +0300

    One more sout added

--- src/main/java/org/example/LeftBranchClass.java
+++ src/main/java/org/example/LeftBranchClass.java
@@ -3,5 +3,6 @@ package org.example;
 public class LeftBranchClass {
     public static void main(String[] args) {
         System.out.println("Left branch has been added to Git Repo");
+        System.out.println("Modified to continue left branch");
     }
 }
