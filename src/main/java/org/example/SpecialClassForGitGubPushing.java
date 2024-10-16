25aebf08703b5ffad13bd23000030fb1cff6573d
AlexanderD <dementyev91@gmail.com>
2024-10-16 07:56:06 +0300

    Syntax mistakes fixed

--- src/main/java/org/example/SpecialClassForGitGubPushing.java
+++ src/main/java/org/example/SpecialClassForGitGubPushing.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class SpecialClassForGitGubPushing {
     public static void main(String[] args) {
-        System.err.println("Yeah, it's realy special class");
-        ystem.err.println("It's for main branch");
+        System.err.println("Yeah, it's really special class");
+        System.err.println("It's for main branch");
     }
 }
